340666b4c8dc94df783e45a3cb4411fdbb3d6c08
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2025-07-09 20:46:59 +0530

    Time: 4 ms (18.83%), Space: 44.1 MB (73.3%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -1,17 +1,34 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-        boolean found=true;
-        while(found){
-            found=false;
-            for(int i=0; i<nums.length; i++){
-            if(original==nums[i]){
-                original = 2*original;
-                found=true;
-                break;
-            }
-            }
+        for(int i=0;i<nums.length;i++){
+             int res=binarySearch(nums,original);
+        if(res==-1){
+            return original;
+        }
+        else{
+            original=2*original;
+        }
         }
         return original;
         
+    }
+
+     public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        Arrays.sort(arr);
+        while (left <= right) {
+            int mid = left + (right - left) / 2; 
+
+            if (arr[mid] == target) {
+                return mid; 
+            } else if (arr[mid] < target) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
+            }
+        }
+
+        return -1; 
     }
 }
