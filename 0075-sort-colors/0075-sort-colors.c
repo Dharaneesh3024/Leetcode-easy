e90e4b514d21154cce877f7775117184a11eecf1
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2025-08-23 15:31:17 +0530

    Time: 0 ms (100%), Space: 9.8 MB (63.37%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.c
@@ -0,0 +1,12 @@
+void sortColors(int* nums, int numsSize) {
+    int temp=0;
+    for(int i=0;i<numsSize;i++){
+        for(int j=0;j<numsSize;j++){
+            if(nums[i]<nums[j]){
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+        }
+    }
+}
