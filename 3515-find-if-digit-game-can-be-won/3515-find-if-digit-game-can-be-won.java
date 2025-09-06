5f121e6af2d62b3b17ea4ccbeae106ff189db05c
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2025-09-06 19:05:40 +0530

    Time: 0 ms (100%), Space: 44 MB (94.05%) - LeetHub

--- 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.java
@@ -1,15 +1,10 @@
 class Solution {
     public boolean canAliceWin(int[] nums) {
-       int s=0; 
-       int d=0; 
+       int s=0;int d=0; 
        for(int i:nums){
             if(i>=10){
-                d+=i;
-            }
+                d+=i;}
             else{
-                s+=i;
-            }
+                s+=i;}
        }
-       return (!(s==d));
-    }
-}
+       return (!(s==d)); }}
