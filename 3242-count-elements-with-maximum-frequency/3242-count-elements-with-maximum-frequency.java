8f283801bc2db69eca6a1f8b7ead029ded1662dc
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2025-09-22 15:47:22 +0530

    Time: 2 ms (75.64%), Space: 42.2 MB (86.43%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -3,19 +3,14 @@ class Solution {
         int arr[]=new int[101];
         int res=0;
         for(int i:nums){
-            arr[i]++;
-        }
+            arr[i]++;}
         int max_val=-1;
         for(int i:arr){
             if(i>max_val){
                 max_val=i;
-            }
-        }
+            }}
         for(int i:arr){
             if(i==max_val){
                 res+=i;
-            }
-        }
-    return res;
-    }
-}
+            }}
+    return res;}}
