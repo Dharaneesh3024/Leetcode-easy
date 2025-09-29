7eb4a8a3c1c828a1357cb77f8d5ee546bf5bf652
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2025-09-29 15:35:54 +0530

    Time: 1 ms (100%), Space: 44 MB (97.8%) - LeetHub

--- 3533-snake-in-matrix/3533-snake-in-matrix.java
+++ 3533-snake-in-matrix/3533-snake-in-matrix.java
@@ -1,28 +1,26 @@
 class Solution {
     public int finalPositionOfSnake(int n, List<String> commands) {
-        int arr[][]=new int[n][n];
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                arr[i][j]=count;
-                count++;
-            }}
-            int ind1=0,ind2=0;
-        int res=arr[ind1][ind2];
-            for(String s:commands){
-                if(s.equals("UP")){
-                ind1--;
-                }
-                else if(s.equals("DOWN")){
-                    ind1++;
-                }
-                else if(s.equals("LEFT")){
-                    ind2--;
-                }
-                else if(s.equals("RIGHT")){
-                    ind2++;
-                }
+        int row = 0;
+        int col = 0;
+
+        for (String s : commands) {
+            char c = s.charAt(0);
+
+            switch (c) {
+                case 'U':
+                    row--;
+                    break;
+                case 'D':
+                    row++;
+                    break;
+                case 'R':
+                    col++;
+                    break;
+                case 'L':
+                    col--;
+                    break;
             }
-            return arr[ind1][ind2];
+        }
+        return (row * n) + col;
     }
 }
