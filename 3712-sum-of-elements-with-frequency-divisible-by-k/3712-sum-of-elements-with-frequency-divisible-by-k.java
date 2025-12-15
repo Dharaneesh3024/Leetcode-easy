79db85aad040b83590bc653a43ada7f72c360906
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2025-12-15 20:42:30 +0530

    Time: 3 ms (54.11%), Space: 44.1 MB (24.26%) - LeetHub

--- 3712-sum-of-elements-with-frequency-divisible-by-k/3712-sum-of-elements-with-frequency-divisible-by-k.java
+++ 3712-sum-of-elements-with-frequency-divisible-by-k/3712-sum-of-elements-with-frequency-divisible-by-k.java
@@ -3,9 +3,16 @@ class Solution {
         Map<Integer, Integer> hm = new HashMap<>();
         int sum = 0;
         for (int num : nums) {
-            hm.put(num, hm.getOrDefault(num, 0) + 1);}
-        for (int num : nums) {
-            if (hm.get(num) % k == 0) {
-                sum += num;
-            } }
-        return sum; }}
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : hm.entrySet()) {
+            int num = entry.getKey();
+            int freq = entry.getValue();
+
+            if (freq % k == 0) {
+                sum += num * freq;
+            }
+        }
+        return sum;
+    }
+}
