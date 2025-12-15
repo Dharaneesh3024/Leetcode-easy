ed58b9299fb5419db1f2c2104f6016e9cdf6280b
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2025-12-15 20:41:28 +0530

    Time: 3 ms (54.11%), Space: 44.4 MB (8.16%) - LeetHub

---
+++ 3712-sum-of-elements-with-frequency-divisible-by-k/3712-sum-of-elements-with-frequency-divisible-by-k.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumDivisibleByK(int[] nums, int k) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        int sum = 0;
+        for (int num : nums) {
+            hm.put(num, hm.getOrDefault(num, 0) + 1);
+        }
+        for (int num : nums) {
+            if (hm.get(num) % k == 0) {
+                sum += num;
+            }
+        }
+
+        return sum;
+    }
+}
