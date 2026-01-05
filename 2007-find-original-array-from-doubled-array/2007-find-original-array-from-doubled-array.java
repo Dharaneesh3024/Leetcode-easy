c4378130b124b966b6aba66f75204ef3c19a201a
Dharaneesh A <153302869+Dharaneesh3024@users.noreply.github.com>
2026-01-05 21:05:25 +0530

    Time: 16 ms (93.85%), Space: 124.6 MB (97.31%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        int n = changed.length;
+        if (n % 2 != 0) return new int[0];
+
+        int[] cnt = new int[200001];
+        for (int x : changed) cnt[x]++;
+
+        int[] ans = new int[n / 2];
+        int idx = 0;
+
+        for (int i = 0; i < cnt.length; i++) {
+            while (cnt[i] > 0) {
+                // if double does not exist
+                if (i * 2 >= cnt.length || cnt[i * 2] <= 0) {
+                    return new int[0];
+                }
+
+                cnt[i]--;
+                cnt[i * 2]--;
+                ans[idx++] = i;
+            }
+        }
+        return ans;
+    }
+}
